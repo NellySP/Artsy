cff29f9ff013602e2ec77ae353efb29035172e7f
NellySP <nelly.petren@gmail.com>
2022-03-08 10:52:53 +0100

    fix login
    sort of, we can see dashboard now

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
+// Route::get('/', function () {
+//     return view('index');
+// });
 // Route::view('/', 'index')->name('login')->middleware('guest');
+Route::view('/', 'index');
 Route::post('login', LoginController::class);
 Route::get('dashboard', DashboardController::class)->middleware('auth');
