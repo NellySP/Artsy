bbf5fd3f892f2acd87d71abd84e3583755975540
NellySP <nelly.petren@gmail.com>
2022-03-08 11:42:43 +0100

    user page

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,3 +2,6 @@
 
 <p>Hello {{$user->name}}!</p>
 <p>Do you want to <a href="logout">logout?</a></p>
+
+<p>view your page</p>
+<button> <a href="user-page.php">HERE</a></button>
---
+++ resources/views/user-page.blade.php
@@ -0,0 +1,2 @@
+<h2>My favorite art</h2>
+<p>right here below!</p>
