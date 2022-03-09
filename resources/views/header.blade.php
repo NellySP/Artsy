c14cd790508ac215cf10bd2c5eb6d10ca39288a5
NellySP <nelly.petren@gmail.com>
2022-03-09 15:35:05 +0100

    css

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,4 +1,17 @@
-<!-- some kind of logo or site name -->
-<h1>Artsy</h1>
+<!DOCTYPE html>
+<html lang="en">
 
-@include('navigation')
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/public/Css/index.css">
+    <title>Artsy</title>
+</head>
+
+<body>
+
+    <!-- some kind of logo or site name -->
+    <h1>Artsy</h1>
+
+    @include('navigation')
