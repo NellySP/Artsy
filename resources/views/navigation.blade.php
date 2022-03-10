457d12814a98dece2998e66878c5d0568a1a34b7
NellySP <nelly.petren@gmail.com>
2022-03-10 21:46:34 +0100

    rearrange navigation

--- resources/views/navigation.blade.php
+++ resources/views/navigation.blade.php
@@ -6,13 +6,13 @@
             <!-- Hide when logged in -->
             <li><a href="signup">Signup</a></li>
             @endif
-            <!-- hide when not logged in -->
             @if (Auth::user())
+            <!-- hide when not logged in -->
             <li><a href="exhibitions">Exhibitions</a></li>
             <li><a href="user-page">My page</a></li>
+            @endif
             <!-- show only to admin -->
             <li><a href="admin">Admin</a></li>
-            @endif
         </ul>
     </div>
 </nav>
