6dfd2f87dfba652cc72f622bc906c408f3e8b111
NellySP <nelly.petren@gmail.com>
2022-03-10 21:09:08 +0100

    add nav view
    hide login when logged in and vice versa

--- resources/views/navigation.blade.php
+++ resources/views/navigation.blade.php
@@ -2,13 +2,17 @@
     <div>
         <ul>
             <li><a href="dashboard">Home</a></li>
+            @if (Auth::guest())
             <!-- Hide when logged in -->
             <li><a href="signup">Signup</a></li>
+            @endif
             <!-- hide when not logged in -->
+            @if (Auth::user())
             <li><a href="exhibitions">Exhibitions</a></li>
             <li><a href="user-page">My page</a></li>
             <!-- show only to admin -->
             <li><a href="admin">Admin</a></li>
+            @endif
         </ul>
     </div>
 </nav>
