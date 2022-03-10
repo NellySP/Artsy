a3a79d686b1ed6ebb7131f508edf6ae33f9402b4
NellySP <nelly.petren@gmail.com>
2022-03-10 13:19:58 +0100

    add artwork info

--- resources/views/exhibitions.blade.php
+++ resources/views/exhibitions.blade.php
@@ -2,28 +2,18 @@
 
 <h2>Look at all those exhibitions!</h2>
 
+<p>list of all exhibitions that leads to a single exhibition page?</p>
 
 @foreach ($images as $image)
 <div>
-    <p>{{$image->title}}</p>
-    <img src="{{ $image->image }}" alt="picture of art">
+    <img src="/images/{{ $image->image }}" alt="picture of art">
+    <p>Artwork: {{$image->title}}</p>
+    <p>Artist: {{$image->artist}}</p>
+    <p>Exhibition: {{$image->exhibition}}</p>
+
 </div>
 
 @endforeach
 
-<p>list of all exhibitions that leads to a single exhibition page?</p>
-
-<?php
-
-use Illuminate\Support\Facades\DB;
-
-$photos = DB::select('select * from photos'); ?>
-
-<ul>
-
-    @foreach ($photos as $photo)
-    <li>{{$photo->title}}<br><img src="./images/<?= $photo->filename ?>" alt=""></button> </li>
-    @endforeach
-</ul>
 
 @include('footer')
