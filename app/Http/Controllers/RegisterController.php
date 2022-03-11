81b6c6cc1a49dbf895df3eec188254aed661c12a
NellySP <nelly.petren@gmail.com>
2022-03-11 10:52:23 +0100

    add welcome email
    on registration via mailtrap

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
 
 class RegisterController extends Controller
 {
@@ -20,6 +21,19 @@ class RegisterController extends Controller
         $user->email = $data['email'];
         $user->password = Hash::make($data['password']);
         $user->save();
+
+        // email data
+        $email_data = array(
+            'name' => $data['name'],
+            'email' => $data['email'],
+        );
+
+        // send email with the template
+        Mail::send('welcome_email', $email_data, function ($message) use ($email_data) {
+            $message->to($email_data['email'], $email_data['name'])
+                ->subject('Welcome to Artsy')
+                ->from('info@artsy.com', 'Artsy');
+        });
         return redirect("/");
     }
 }
---
+++ resources/views/welcome_email.blade.php
@@ -0,0 +1,6 @@
+Hello {{ $name }},<br><br>
+
+Welcome to Artsy.<br><br>
+
+Thank You,<br>
+Artsy
