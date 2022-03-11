f50fac921ac05f74267214cd97b500f98b893ad1
NellySP <nelly.petren@gmail.com>
2022-03-11 16:26:48 +0100

    single ex progress

--- app/Http/Controllers/SingleexhibitionController.php
+++ app/Http/Controllers/SingleexhibitionController.php
@@ -3,13 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Models\Exhibition;
+use App\Models\Image;
 use Illuminate\Http\Request;
 
 class SingleexhibitionController extends Controller
 {
     public function __invoke(Request $request, string $id)
     {
+        $images = Image::where('exhibition_id', '=', $_GET[$id])->first();
+        // $images = Image::where('exhibition_id', '=', $id)->first();
         $exhibition = Exhibition::where('id', '=', $id)->first();
-        return view('singleexhibition', ['exhibition' => $exhibition]);
+        return view('singleexhibition', ['exhibition' => $exhibition, 'images' => $images]);
     }
 }
