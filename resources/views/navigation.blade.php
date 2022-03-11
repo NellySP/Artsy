91690515aaa5130edc9f284750db715f1bcdbd82
NellySP <nelly.petren@gmail.com>
2022-03-11 15:19:35 +0100

    Update navigation.blade.php

--- resources/views/navigation.blade.php
+++ resources/views/navigation.blade.php
@@ -1,17 +1,17 @@
 <nav>
     <div>
         <ul>
-            <li><a href="dashboard">Home</a></li>
+            <li><a href="/dashboard">Home</a></li>
             @if (Auth::guest())
             <!-- Hide when logged in -->
-            <li><a href="signup">Signup</a></li>
+            <li><a href="/signup">Signup</a></li>
             @endif
             @if (Auth::user())
             <!-- hide when not logged in -->
-            <li><a href="exhibitions">Exhibitions</a></li>
-            <li><a href="user-page">My page</a></li>
+            <li><a href="/exhibitions">Exhibitions</a></li>
+            <li><a href="/user-page">My page</a></li>
             <!-- show only to admin -->
-            <li><a href="admin">Admin</a></li>
+            <li><a href="/admin">Admin</a></li>
             @endif
         </ul>
     </div>
