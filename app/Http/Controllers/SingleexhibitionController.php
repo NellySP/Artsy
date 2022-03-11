857a035e87064fdb37a7ddcd0dae1a76da945453
NellySP <nelly.petren@gmail.com>
2022-03-11 17:21:53 +0100

    clean up comments

--- app/Http/Controllers/SingleexhibitionController.php
+++ app/Http/Controllers/SingleexhibitionController.php
@@ -10,10 +10,8 @@ class SingleexhibitionController extends Controller
 {
     public function __invoke(Request $request, string $id)
     {
-        // this does not work >:( 
+
         $images = Image::where('exhibition_id', '=', $id)->get();
-        // neither does this
-        // $images = Image::where('exhibition_id', '=', $_GET[$id]);
         $exhibition = Exhibition::where('id', '=', $id)->first();
         return view('singleexhibition', ['exhibition' => $exhibition, 'images' => $images]);
     }
