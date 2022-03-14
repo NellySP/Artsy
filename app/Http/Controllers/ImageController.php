a20c271b703a85a857b9b4fadde3f18e6e850d75
NellySP <nelly.petren@gmail.com>
2022-03-14 21:46:08 +0100

    clean up ImageController

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -12,13 +12,9 @@ class ImageController extends Controller
 {
     public function __invoke(Request $request)
     {
-        //  kan detta funka på nåt sätt eller måste vi lägga till
-        // bildens filnamn i like-bordet? Isåfall gör vi det. 
         $user = Auth::user();
-        $images = Image::where('id', '=', $user->like['image_id'])->get();
-
         return view('user-page', [
-            'user' => $user, 'images' => $images,
+            'user' => $user,
         ]);
     }
 }
