53ef51bcd3237d6f356eb42626474822c97a02e2
NellySP <nelly.petren@gmail.com>
2022-03-14 17:04:04 +0100

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,7 +3,7 @@
 <h1>hej Dashboard</h1>
 
 <p>Hello {{$user->name}}!</p>
-<p>Do you want to <a href="logout">logout?</a></p>
+<p>Do you want to <button href="logout">logout?</button></p>
 
 <p>view your page</p>
 <button> <a href="user-page">HERE</a></button>
