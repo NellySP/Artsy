2fe40ce4fab705b2bf4a58f09c0a0d4b7ba8d431
NellySP <nelly.petren@gmail.com>
2022-03-14 20:48:36 +0100

    not working display favs

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -3,15 +3,21 @@
 namespace App\Http\Controllers;
 
 use App\Models\Image;
+use App\Models\Like;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ImageController extends Controller
 {
     public function __invoke(Request $request)
     {
+        // $likes = Like::all();
+        $user = Auth::user();
+        // $userFavorites = $user->like;
+        $images = Image::where('id', '=', $user->like)->get();
 
-        $images = Image::all();
-
-        return view('exhibitions', ['images' => $images]);
+        return view('user-page', [
+            'user' => $user, 'images' => $images,
+        ]);
     }
 }
--- resources/views/user-page.blade.php
+++ resources/views/user-page.blade.php
@@ -4,5 +4,10 @@
 
 <p>on this page you'll see a selection of pieces that you've marked with a like ♡</p>
 
+@foreach($images as $image)
+<img src="/images/{{$image -> image}}" alt="picture of art">
+<p>{{$image->image_id}}</p>
+
+@endforeach
 
 @include('footer')
