66cd2e517f93eafbd04fa2944db9f04745e1ba43
NellySP <nelly.petren@gmail.com>
2022-03-15 17:18:28 +0100

    add a little div
    why not

--- resources/views/user-page.blade.php
+++ resources/views/user-page.blade.php
@@ -4,12 +4,13 @@
 
 <p>on this page you'll see a selection of pieces that you've marked as liked </p>
 
-<ul>
-
-    @foreach($user->like as $like)
-    <img src="/images/{{$like -> image}}" alt="picture of art">
-    <p>{{$like->image_name}}</p>
-    @endforeach
-</ul>
+<div>
+    <ul>
+        @foreach($user->like as $like)
+        <img src="/images/{{$like -> image}}" alt="picture of art">
+        <p>{{$like->image_name}}</p>
+        @endforeach
+    </ul>
+</div>
 
 @include('footer')
